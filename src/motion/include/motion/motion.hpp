015b9234f72b37dbd9f4bbeb87ad4750b5aba1c4
Chris-G32 <cgruska@kent.edu>
2023-11-28 16:40:42 -0500

    Contains data types in and out of the pkg

---
+++ src/motion/include/motion/motion.hpp
@@ -0,0 +1,14 @@
+#ifndef MOTION_HPP
+#define MOTION_HPP
+
+namespace Motion
+{
+    enum MovementType
+    {
+        POSITION = 'P',
+        PWM = 'T',
+        VELOCITY = 'V'
+    };
+}
+
+#endif
