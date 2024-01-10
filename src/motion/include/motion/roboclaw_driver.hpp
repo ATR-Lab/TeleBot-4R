33f95e2b01904a32f059e5a7d27bdc2955acff94
NerosJourney <sandman4264@gmail.com>
2024-01-10 13:52:14 -0500

    Roboclaw driver first stage

--- src/motion/include/motion/roboclaw_driver.hpp
+++ src/motion/include/motion/roboclaw_driver.hpp
@@ -16,37 +16,53 @@
 #ifndef ROBOCLAW_DRIVER_HPP
 #define ROBOCLAW_DRIVER_HPP
 
+#define DEFAULT_ADDRESS 128
+
 #include "motion/driver.hpp"
 #include "motion/motion.hpp"
 
-#include "roboclaw_sdk/roboclaw_driver.h"
+#include "roboclaw_sdk/driver.h"
 
 #include <unordered_map>
 #include <vector>
+#include <string>
 
 class RoboclawDriver : public MotorDriver {
 public:
     RoboclawDriver(): MotorDriver() {}
 
-    RoboclawDriver(int baudrate, std::vector<int> motorIDs) : MotorDriver(), _port(port),
-                                                                           _baudrate(baudrate), _motorIDs(motorIDs) {}
+    RoboclawDriver(int baudrate, std::unordered_map<int, std::string> motorPorts) : MotorDriver(), _baudrate(baudrate), _motorPorts(motorPorts) {}
 
-    int initialize() {
-        std::for_each(_motorIDs.cbegin(), _motorIDs.cend(), [_drivers, _baudrate](int id){
-            //TODO: figure out the ports..?
-            driver temp = new driver("tempPort", _baudrate);
+    int initialize() override {
+        std::for_each(_motorPorts.cbegin(), _motorPorts.cend(), [this](std::pair<int, std::string> val){
+            driver temp(val.second, _baudrate);
+            int id = val.first;
             // This might not be the best idea, not sure how it will play out...
-            temp.reset_encoders();
-            _drivers[id] = temp;
-        })
+            temp.reset_encoders(DEFAULT_ADDRESS);
+            // Creates the pair and inserts it
+            _drivers.emplace(id, temp);
+        });
+    }
+
+protected:
+    void writeMotors() override {
+        auto goals = getMotorGoals();
+
+        for(auto &goal : goals->motor_goals) {
+            // NOTE: the speed parameter of this set_velocty might be wrong. It mentions that it should be big endian.
+            _drivers[goal.motor_id].set_velocity(DEFAULT_ADDRESS, std::make_pair(goal.motor_goal, goal.motor_goal));
+        }
+    }
+
+    MotorStateList readMotors() override {
+        return MotorStateList();
     }
 
 private:
     std::unordered_map<int, driver> _drivers;
 
-    std::vector<int> _motorIDs;
+    std::unordered_map<int, std::string> _motorPorts;
     int _baudrate;
-
-}
+};
 
 #endif
